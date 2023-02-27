b35ba592b408e589d22ca464727a411a20355f7c
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-02-27 17:23:54 -0500

    Transformer: call method developed to creat multi head attention structure.

--- Code/multiheadAttention.py
+++ Code/multiheadAttention.py
@@ -11,3 +11,9 @@ class multiheadAttention(tf.keras.layers.Layer):
         random = tf.random_normal_initializer()
         self.Wo = tf.Variable(initial_value = random(shape = [self.nHead*self.Dv,inputDim[0][-1]],dtype='float'),
                             trainable=True,name = 'query weights')
+    def call(self,inputs):
+        heads = []
+        for i in range(self.nHead):
+            heads.append(scaledDotProductAttention(Dv = self.Dv,Dk = self.Dk)(inputs))
+        multiHead = tf.concat(heads,axis = -1)
+        return tf.matmul(multiHead,self.Wo)

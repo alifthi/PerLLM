0f871e8992121dfea47f06f6f7f81b8da7d81b4e
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-01 20:23:50 -0500

    Transformer: call method developed to creat Encoder

--- Code/Encoder.py
+++ Code/Encoder.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from multiheadAttention import multiheadAttention 
 from tensorflow.keras import layers as ksl
-from tensorflow.keras import layers as ksl
 class Encoder(tf.keras.layers.Layer):
     def __init__(self,denseDim = 2048,numEncoder = 6,Dv = 64,Dk = 512,nHead = 8):
         super().__init__()
@@ -12,3 +11,14 @@ class Encoder(tf.keras.layers.Layer):
         self.denseDim = denseDim            # dimention of middle feed forward network    
     def build(self,inputShape):
         self.attention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,nHead=self.nHead)
+    def call(self,inputs):
+        y = inputs
+        for _ in range(self.numEncoder):
+            x = self.attention([y,y,y])
+            x =  ksl.Add()([y,x])
+            x =  ksl.LayerNormalization()(x)
+            ff = ksl.Dense(self.denseDim,activation = 'relu')(x)
+            ff = ksl.Dense(inputs.shape[-1])(ff)
+            x =  ksl.Add()([ff,x])
+            y =  ksl.LayerNormalization()(x)
+        return y

72d8e4cbf6c75b8939764794788c0f6bbddda437
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-03 12:10:07 -0500

    Transformer: call method developed to building decoder structure

--- Code/Decoder.py
+++ Code/Decoder.py
@@ -16,3 +16,19 @@ class Decoder(tf.keras.layers.Layer):
             self.attention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,
                                                       nHead=self.nHead)
             self.posEncoding = self.encoder(inputShape=inputShape[1])
+    def call(self,inputs):
+        from tensorflow.keras import layers as ksl
+        y = inputs[0] + self.posEncoding
+        for _ in range(self.numEncoder):
+            x = self.maskedAttention([y,y,y])
+            x =  ksl.Add()([y,x])
+            x =  ksl.LayerNormalization()(x)
+            encoderOut = self.encoder(inputs[1])
+            attention = self.attention([x,encoderOut,encoderOut])
+            x =  ksl.Add()([attention,x])
+            x =  ksl.LayerNormalization()(x)
+            ff = ksl.Dense(self.denseDim,activation = 'relu')(x)
+            ff = ksl.Dense(inputs.shape[-1])(ff)
+            x =  ksl.Add()([ff,x])
+            y =  ksl.LayerNormalization()(x)
+        return y

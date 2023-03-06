bd1401ab612afd2e1533006a18486e4fc1997546
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-06 18:56:40 -0500

    Transformer: tozenize the article corpus

--- Code/utils.py
+++ Code/utils.py
@@ -13,3 +13,6 @@ class utils:
         data['article'] = data['article'].str.replace('[^\w\s]','')
         data['article'] = data['article'].str.replace('\([^)]*\)','')
         data['highlights'] = '_start_' + ' ' + data['highlights'].str.replace('[^\w\s]','') + ' ' + '__end__'
+        tok = tf.keras.preprocessing.text.Tokenizer()
+        tok.fit_on_texts(list(data['article'].astype(str))) 
+        text = tok.texts_to_sequences(list(data['article'].astype(str)))

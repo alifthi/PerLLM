7ef38c45e3d270235c6378571d9065f36300b2e6
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-06 18:49:24 -0500

    Transformer: loadData method developed for loading train and test data.

---
+++ Code/utils.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+class utils:
+    def __init__(self,dataDir):
+        self.dataDir = dataDir
+    def loadData(self):
+        self.trainData = pd.read_csv(self.dataDir + 'train.csv')
+        self.testData = pd.read_csv(self.dataDir + 'test.csv')

290ceba56edeed8fb6ef355f861d4378f062ba03
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-06 19:40:54 -0500

    Transformer: some changes applied to decoder input mechanisem

--- Code/Decoder.py
+++ Code/Decoder.py
@@ -8,14 +8,13 @@ class Decoder(tf.keras.layers.Layer):
         self.Dv = Dv
         self.nHead = nHead                  # number of heads in multihead attention
         self.denseDim = denseDim
-        self.encoder = encoder()
     def build(self,inputShape):
-            from multiheadAttention import multiheadAttention 
-            self.maskedAttention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,
-                                                      nHead=self.nHead,masked=True)
-            self.attention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,
-                                                      nHead=self.nHead)
-            self.posEncoding = self.encoder(inputShape=inputShape[1])
+        from multiheadAttention import multiheadAttention 
+        self.maskedAttention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,
+                                                    nHead=self.nHead,masked=True)
+        self.attention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,
+                                                    nHead=self.nHead)
+        self.posEncoding = self.posEncoder(inputShape=inputShape[1])
     def call(self,inputs):
         from tensorflow.keras import layers as ksl
         y = inputs[0] + self.posEncoding
@@ -23,7 +22,7 @@ class Decoder(tf.keras.layers.Layer):
             x = self.maskedAttention([y,y,y])
             x =  ksl.Add()([y,x])
             x =  ksl.LayerNormalization()(x)
-            encoderOut = self.encoder(inputs[1])
+            encoderOut = inputs[1] # self.encoder(inputs[1])
             attention = self.attention([x,encoderOut,encoderOut])
             x =  ksl.Add()([attention,x])
             x =  ksl.LayerNormalization()(x)
@@ -32,3 +31,12 @@ class Decoder(tf.keras.layers.Layer):
             x =  ksl.Add()([ff,x])
             y =  ksl.LayerNormalization()(x)
         return y
+    def posEncoder(self,inputShape):
+        import numpy as np
+        posEncoding = np.zeros((inputShape[0], self.Dk))
+        for k in range(inputShape[0]):
+            for i in np.arange(int(self.Dk/2)):
+                denominator = np.power(10000, 2*i/self.Dk)
+                posEncoding[k, 2*i] = np.sin(k/denominator)
+                posEncoding[k, 2*i+1] = np.cos(k/denominator)
+        return posEncoding

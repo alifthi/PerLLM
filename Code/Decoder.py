e38fd5f18abeee8eaa48e5bc8564c37fe605afa0
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-10 21:42:10 -0500

    Transformer: some changes applied on model.

--- Code/Decoder.py
+++ Code/Decoder.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from Encoder import Encoder as encoder
 class Decoder(tf.keras.layers.Layer):
-    def __init__(self,denseDim = 2048,numDecoder = 6,Dv = 64,Dk = 512,nHead = 8):
+    def __init__(self,denseDim = 2048,numDecoder = 6,Dv = 64,Dk = 256,nHead = 8):
         super().__init__()
         self.numDecoder = numDecoder        # number of encoders
         self.Dk = Dk                        
@@ -18,7 +18,7 @@ class Decoder(tf.keras.layers.Layer):
     def call(self,inputs):
         from tensorflow.keras import layers as ksl
         y = inputs[0] + self.posEncoding
-        for _ in range(self.numEncoder):
+        for _ in range(self.numDecoder):
             x = self.maskedAttention([y,y,y])
             x =  ksl.Add()([y,x])
             x =  ksl.LayerNormalization()(x)
@@ -27,14 +27,14 @@ class Decoder(tf.keras.layers.Layer):
             x =  ksl.Add()([attention,x])
             x =  ksl.LayerNormalization()(x)
             ff = ksl.Dense(self.denseDim,activation = 'relu')(x)
-            ff = ksl.Dense(inputs.shape[-1])(ff)
+            ff = ksl.Dense(y.shape[-1])(ff)
             x =  ksl.Add()([ff,x])
             y =  ksl.LayerNormalization()(x)
         return y
     def posEncoder(self,inputShape):
         import numpy as np
-        posEncoding = np.zeros((inputShape[0], self.Dk))
-        for k in range(inputShape[0]):
+        posEncoding = np.zeros((inputShape[1], self.Dk))
+        for k in range(inputShape[1]):
             for i in np.arange(int(self.Dk/2)):
                 denominator = np.power(10000, 2*i/self.Dk)
                 posEncoding[k, 2*i] = np.sin(k/denominator)
--- Code/model.py
+++ Code/model.py
@@ -3,9 +3,10 @@ from Decoder import Decoder as decoder
 from tensorflow.keras import layers as ksl
 import tensorflow as tf
 class model:
-    def __init__(self,inputSize,latentDim = 256):
+    def __init__(self,inputSize,decoderVocabSize,latentDim = 256):
         self.inputSize = inputSize
         self.latentDim = latentDim
+        self.decoderVocabSize = decoderVocabSize
     def buildModel(self):
         encoderInput = ksl.Input(self.inputSize[0])
         x = ksl.Embedding(self.inputSize[0],
@@ -14,13 +15,13 @@ class model:
         x = ksl.BatchNormalization()(x)
         encoderOutput = encoder()(x)
 
-        decoderInput = ksl.Input(shape = [None,])
-        x  = ksl.Embedding(self.inputSize[1],
+        decoderInput = ksl.Input(self.inputSize[0])
+        x  = ksl.Embedding(self.decoderVocabSize,
                            self.latentDim,
-                           mask_zero = False)
+                           mask_zero = False)(decoderInput)
         x = decoder()([x,encoderOutput])
         x = ksl.BatchNormalization()(x)
-        x = ksl.Dense(self.inputSize[1],activation = 'softmax')(x)
+        x = ksl.Dense(self.decoderVocabSize,activation = 'softmax')(x)
         model = tf.keras.Model([encoderInput,decoderInput],x)
         return model
     def compileModel(self):

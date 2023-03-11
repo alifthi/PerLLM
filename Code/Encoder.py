f699feefaba35e3242a2560db4707cec1e47715c
Ali Fathi Jahromi <alifathi8008@gmail.com>
2023-03-10 21:32:53 -0500

    Transformer: some changes applied and batch dimension considered

--- Code/Encoder.py
+++ Code/Encoder.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 class Encoder(tf.keras.layers.Layer):
-    def __init__(self,denseDim = 2048,numEncoder = 6,Dv = 64,Dk = 512,nHead = 8):
+    def __init__(self,denseDim = 2048,numEncoder = 6,Dv = 64,Dk = 256,nHead = 8):
         super().__init__()
         self.numEncoder = numEncoder        # number of encoders
         self.Dk = Dk                        
@@ -8,14 +8,14 @@ class Encoder(tf.keras.layers.Layer):
         self.nHead = nHead                  # number of heads in multihead attention
         self.denseDim = denseDim            # dimention of middle feed forward network    
     def build(self,inputShape):
-        from multiheadAttention import multiheadAttention 
-        self.attention = multiheadAttention(Dk = self.Dk,Dv = self.Dv,nHead=self.nHead)
         self.posEncoding = self.posEncoder(inputShape=inputShape)
+
     def call(self,inputs):
+        from multiheadAttention import multiheadAttention 
         from tensorflow.keras import layers as ksl
         y = inputs + self.posEncoding
         for _ in range(self.numEncoder):
-            x = self.attention([y,y,y])
+            x =  multiheadAttention(Dk = self.Dk,Dv = self.Dv,nHead=self.nHead)([y,y,y])
             x =  ksl.Add()([y,x])
             x =  ksl.LayerNormalization()(x)
             ff = ksl.Dense(self.denseDim,activation = 'relu')(x)
@@ -25,8 +25,8 @@ class Encoder(tf.keras.layers.Layer):
         return y
     def posEncoder(self,inputShape):
         import numpy as np
-        posEncoding = np.zeros((inputShape[0], self.Dk))
-        for k in range(inputShape[0]):
+        posEncoding = np.zeros((inputShape[1], self.Dk))
+        for k in range(inputShape[1]):
             for i in np.arange(int(self.Dk/2)):
                 denominator = np.power(10000, 2*i/self.Dk)
                 posEncoding[k, 2*i] = np.sin(k/denominator)
